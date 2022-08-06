5daa4b77acddbdc81fb69fcf7a9e61a6cbbb3eab
mennaAFathy1014 <menna10awaden@gmail.com>
2022-08-06 09:08:38 +0200

    testing

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,38 +1,111 @@
-let arrayOfElements = document.querySelectorAll('.calc-body-cell');
-let elements = document.querySelector('.calc-body');
-let output = document.querySelector('.output');
- 
-elements.addEventListener('click', function(event) {
-    let res = false;
-    let number = event.target.innerHTML;
-        if (event.target.classList.contains('calc-body-cell') && !event.target.classList.contains('del') && !event.target.classList.contains('res') && !event.target.classList.contains('equal') ){ 
-            if(res == false){
-                output.innerHTML += number;
-            }
-            else if(res == true){
-                reset();
-                output.innerHTML += number;
-                res = false;
-            }
-        }
-        if (event.target.classList.contains('del')){
-            del();
-        }
-        if(event.target.innerHTML =='='){
-            output.innerHTML = eval(output.innerHTML);
-            res = true;
-        }
-        if(event.target.innerHTML =='Reset'){
-            reset();
-        }
-});
-function reset(){
-    output.innerHTML = '';
-}
-function del(){
-    if(output.innerHTML.length > 0){
-        let del = output.innerHTML.split('+'||'-'||'×'||'/').pop();
-        output.innerHTML = output.innerHTML.replace(del,'');
-        output.innerHTML = output.innerHTML.replace('+'||'-'||'×'||'/','');
+class Calc{
+    constructor(preval, currval){
+        this.preval = preval;
+        this.currval = currval;
+        this.reset();
+        console.log(this);
     }
+  reset(){
+    this.currentOp = '';
+    this.previousOp = '';
+    this.operator = undefined;
+  }
+  delete(){
+    this.currentOp = this.currentOp.toString().slice(0, -1);
+
+  }
+  addNumber(number){
+      if(number === '.' && this.currentOp.includes('.')) return
+      this.currentOp = this.currentOp.toString() + number.toString();
 }
+  chooseOperator(operator){
+    if(this.currentOp === '')return
+    if(this.currentOp !== ''){
+      this.equals();
+    }
+    this.operator = operator;
+    this.previousOp = this.currentOp ;
+    this.currentOp = '';
+    console.log(this.operator)
+
+  }
+  equals(){
+    let result;
+    const prev = parseFloat(this.previousOp); 
+    const curr = parseFloat(this.currentOp);
+    if(isNaN(prev) || isNaN(curr)) return
+    switch(this.operator){
+      case '+':
+        result = prev+curr;
+        break;
+      case '-':
+        result = prev-curr;
+        break;
+      case '×':
+        result = prev*curr;
+        break;
+      case '/':
+        result = prev/curr;
+        break;
+      default:
+        return
+    }
+    this.currentOp= result;
+    this.operator = undefined;
+    this.previousOp = '';
+    console.log(this.currentOp)
+
+  }
+  update(){
+    this.currval.innerHTML = this.currentOp;
+    if(this.operator !== undefined){
+      this.preval.innerHTML = `${this.previousOp}  ${this.operator}`;
+    }else{
+      this.preval.innerHTML = '';
+    }
+
+  }
+}
+
+
+
+const buttons = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.oper');
+const equals = document.querySelector('.equal');
+const reset = document.querySelector('.res');
+const delet = document.querySelector('.del');
+const currval = document.querySelector('.current');
+const preval = document.querySelector('.previuos');
+
+
+
+const calc = new Calc(preval, currval);
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        calc.addNumber(button.innerHTML);
+        calc.update();
+    } )
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        calc.chooseOperator(operator.innerHTML);
+        calc.update();
+    } )
+})
+
+equals.addEventListener('click', () => {
+    calc.equals();
+    calc.update();
+})
+
+reset.addEventListener('click', () => {
+  calc.reset();
+  calc.update();
+})
+
+delet.addEventListener('click', () => {
+  calc.delete();
+  calc.update();
+})
